528fdd2bb0b9c6dce771447ad3a0389f64bc9df5
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-18 12:30:28 +0530

    add initial implementation of chai-related types and functions

---
+++ src/objectTs.ts
@@ -0,0 +1,110 @@
+const chai = {
+    name: "Masala chai",
+    price: 20,
+    isHot: true
+}
+
+// {
+//     name: string;
+//     price: number;
+//     idHot:boolean  
+// }
+
+
+let tea: {
+    name: string;
+    price: number;
+    isHot: boolean
+}
+tea = {
+    name: "Ginger tea",
+    price: 25,
+    isHot: true
+}
+
+type Tea = {
+    name: string,
+    price: number,
+    ingredients: string[]
+}
+
+const adarkChai: Tea = {
+    name: "Adark Chai",
+    price: 25,
+    ingredients: ['ginger', 'tea leaves']
+}
+
+type Cup = { size: string }
+
+let smallCup: Cup = { size: "200ml" }
+
+let bigCup = { size: "500ml", material: "steel" }
+
+smallCup = bigCup
+
+type Brew = { brewTime: number }
+
+const coffee = { brewTime: 5, beans: "Arabic" }
+const chaiBrew: Brew = coffee
+
+type User = {
+    username: string,
+    password: string
+}
+
+const u: User = {
+    username: "Mann",
+    password: "1234"
+}
+
+type Item = { name: string, quantity: number }
+type Address = { street: string, pin: number }
+
+type Order = {
+    id: string
+    items: Item[]
+    address: Address
+}
+
+type Chai = {
+    name: string
+    price: number
+    isHot: boolean
+}
+
+const updateChai = (updates: Partial<Chai>) => {
+    console.log(`Updating chai with ${updates}`)
+}
+// Partial - value can be change
+
+updateChai({ price: 25 })
+updateChai({ isHot: false })
+// partial can passes empty object als o it  can raise issue
+updateChai({})
+
+type ChaiOrder = {
+    name?: string;
+    quantity?: string
+}
+
+const placeOrder = (order: Required<ChaiOrder>) => {
+    console.log(order)
+}
+placeOrder({
+    name: "Masala Chai",
+    quantity: "2"
+})
+
+type Chai1 = {
+    name: string
+    price: number
+    isHot: boolean
+    ingredients: string[]
+}
+
+type BasicChaiInfo = Pick<Chai1, "name" | "price">
+
+const ChaiInfo: BasicChaiInfo = {
+    name: "Lemon tea",
+    price: 20
+}
