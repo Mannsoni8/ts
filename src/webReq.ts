c17e003e7f5062a4b6c724d1ce94c23dfbdb8eb3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-19 17:24:43 +0530

    add Todo interface and implement fetchData function with error handling

--- src/webReq.ts
+++ src/webReq.ts
@@ -1,6 +1,28 @@
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
+
+interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
 axios.get('https://example.com/data')
     .then(responce => {
         console.log(responce.data)
     })
+
+const fetchData = async () => {
+    try {
+        const res: AxiosResponse<Todo> = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+
+        console.log("Todo", res.data)
+    } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            console.log("Axios Error", error.message)
+            if (error.response) {
+                console.log(error.response.status)
+            }
+        }
+    }
+}
